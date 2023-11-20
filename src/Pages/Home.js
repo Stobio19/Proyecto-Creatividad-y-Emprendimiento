fa920eb8e3a169a83c98ed200665899aefb0ae0f
Adrian Vizacaino <avizcaino@utb.edu.co>
2023-11-20 10:40:44 -0500

    Add fontawesome dependencies and implement password generation feature

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,13 +7,31 @@ export const Home = () => {
   const [phrase, setPhrase] = useState("");
   const [passwordLength, setPasswordLength] = useState("");
   const [showAnnouncement, setShowAnnouncement] = useState(false);
+  const [password, setPassword] = useState(""); 
 
   const togglePopUp = () => {
-    setisOpen(!isOpen);
+    setisOpen(isOpen);
+    setShowAnnouncement(false);
   };
 
-  const handleAnnouncement = () => {
+  const handleAnnouncement = async (event) => {
     if (phrase.trim() !== "" && passwordLength.trim() !== "") {
+      event.preventDefault();
+      fetch("https://generador-de-contrasenas.onrender.com/GENERAR_CONTRASENA", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          frase: phrase,
+          longitud: passwordLength,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setPassword(data);
+        }
+        );
       setShowAnnouncement(true);
       setTimeout(() => {
         setShowAnnouncement(false);
@@ -29,35 +47,41 @@ export const Home = () => {
         <h1>Try Us!</h1>
         <div className="div-main-form-content">
           <h2>Enter Your Data!</h2>
-          <form className="main-form">
+          <form className="main-form"
+          onSubmit={(event) => {
+            handleAnnouncement(event);
+          }
+          }
+          >
             <label>Enter your phrase!</label>
             <input
               placeholder="Phrase"
               required
-
+              onChange={(event) => {
+                const phrase = event.target.value;
+                const spaceCount = (phrase.match(/ /g) || []).length; // Cuenta los espacios en la frase
+                if (spaceCount <= 2) {
+                  setPhrase(phrase); // Actualiza el estado si hay 2 o menos espacios
+                }
+              }}
             ></input>
-            <label>Number greater than 10</label>
+            <label>Number greater than 14</label>
             <input
               placeholder="Number..."
               required
               type="number"
-     
+              min = "14"
+              onChange={(event) => setPasswordLength(event.target.value)}
             ></input>
-            <button
-              onClick={() => {
-                togglePopUp();
-              }}
-              type="submit"
-            >
+            <button type="submit">
               Send
             </button>
             {showAnnouncement && (
               <Modal isOpen={showAnnouncement} className="modal-content">
                 <div className="modal-content-div">
-                  <h2>Modal Window</h2>
+                  <h2>YOUR SECURITY PASSWORD</h2>
                   <div className="content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quibusdam, quod.
+                    <p>{password}</p>
                   </div>
                   <button className="close-modal" onClick={togglePopUp}>
                     Close
